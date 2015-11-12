dbdb42b6d97eb6c02b7e7b37b1c253a934e95a7d
Molly Waggett <mwaggett@alumni.reed.edu>
2015-11-12 10:46:01 -0800

    Add toolbar.

--- app/src/main/java/com/epicodus/materialdesigntutorial/MainActivity.java
+++ app/src/main/java/com/epicodus/materialdesigntutorial/MainActivity.java
@@ -1,16 +1,23 @@
 package com.epicodus.materialdesigntutorial;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     @Override
